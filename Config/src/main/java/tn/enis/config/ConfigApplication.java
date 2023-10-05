0a6dd0288d6bcfbdff8f5f892fd32e1ffcd6001b
ste codily <git@codily.net>
2023-10-05 09:18:51 +0100

    commit conf app

--- Config/src/main/java/tn/enis/config/ConfigApplication.java
+++ Config/src/main/java/tn/enis/config/ConfigApplication.java
@@ -2,6 +2,7 @@ package tn.enis.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
